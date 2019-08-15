9595341d6fb3456e911c3f998f3d0069eea131e0
Claudio Fonseca <claudioribeirofonseca@outlook.com>
2019-08-15 08:27:24 -0300

    Projeto iniciado, mostrando a mensagem Ola Mundo

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _101_Salvando_versões_comandos_add_commit_status_log
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
